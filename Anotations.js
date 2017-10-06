72f302762ff48cfcf0efe51536b7a263e43f7df1
Gabriel Carneiro <g.carneiro@cinq.com.br>
2017-10-06 14:20:19 -0300

    Upload dos códigos

---
+++ Anotations.js
@@ -0,0 +1,78 @@
+/*
+Link Protótipo - Stone
+
+Invision - https://projects.invisionapp.com/share/ZSBKRQFNY#/screens/232201339
+
+Links Página - Home
+
+e-Stone 
+ Docs: http://gateway.stone.com.br/
+
+Stone Online 
+ Docs: http://online.stone.com.br/v1.0/docs
+
+Conciliação Stone 
+ Docs: http://conciliacao.stone.com.br/v1.0/docs
+
+ Credenciamento Stone 
+ Docs: http://credenciamento.stone.com.br/v1.0/docs
+
+FAQ - Contato 
+ Contato: Tela de suporte
+
+Stack Overflow 
+ Acesse: https://pt.stackoverflow.com/
+
+Repositório SDK 
+ Ver no Github: https://github.com/stone-pagamentos/
+
+
+
+
+Modal Sobreposta CSS: 
+ Cor Trasparente: background-color: rgba(0,0,0,0.3);
+
+<html>
+    <head>
+        <title>Teste do div modal</title>
+    </head>
+    <body>
+        <div style="width: 100%; height: 100%; border: solid 3px black; display: flex; align-items: center; justify-content: center; z-index: 0"><a href="www.google.com">Google</a></div>
+        <div style="width: 100%; height: 100%; position: absolute; display: flex; align-items: center; justify-content: center; z-index: 99; background-color: rgba(0,0,0,0.3); left: 0; top: 0">
+        </div>
+    </body>
+</html>
+
+686b6c
+
+
+
+Css For: 
+
+
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+} - input
+
+
+&ensp; - tem o espaço do tamanho de dois espaços normais.
+&emsp; - tem o espaço do tamanho de praticamente quatro espaços normais.
+
+sombra css = 2px 2px 4px #000000;
+
+
+
+Anotações Visual CODE
+
+- Identar: shit + alt + F;
+
+
+*/
+
+
